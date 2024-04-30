81b7080b7ebca6775024d17e4b5229d15e388e85
bassuntrai <bassuntrai@gmail.com>
2024-04-30 12:25:51 +0545

    js basic

--- js/index.js
+++ js/index.js
@@ -1,2 +1,89 @@
-// external
-// internal
+// Data type
+
+// number;
+// string;
+// Boolean;
+// Array;
+// Object;
+
+
+// Operator;
+// ++;
+// --;
+// >
+// < 
+// ==;
+// ===;
+
+
+
+// Keywords
+// const;
+// let;
+// var
+
+// Global scoped
+// var global variable
+var a = 0;
+var a = 5;
+// Can be redeclare 
+
+// block scoped
+// Cannot redeclared in same block
+{
+  let b = "Inside block";
+  // console.log(b)
+}
+let b = "This is outside block";
+// 
+
+
+// Data type
+var x = 12;
+const y = "12";
+
+const z = true;
+const s = undefined;
+
+const o = {
+  value: "aaa"
+}
+
+const add = x + y + z
+// console.log(add)
+
+// Only check structure
+// equalsto ==
+
+// Check type and structure
+// ===
+
+// power
+const opr = x ** 3
+
+// % Modulus
+// x = x % y
+// console.log(x %= y)
+
+// logical operator
+// &&
+// ||
+if (x ?? y == 12) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+// console.log(x || y == 12)
+
+(x = y)
+
+// console.log(x %= y)
+
+
+
+
+
+
+
+
+
